e821cea4ae1520b584400cc2826ebe951aa9bac1
Vishnu <vishnupgithub@gmail.com>
2022-11-26 10:47:43 +0000

    tournament_screen1 updated with umpire and tornament buttons

--- lib/screens/tournament_screen1.dart
+++ lib/screens/tournament_screen1.dart
@@ -14,88 +14,135 @@ class _TournamentScreen1State extends State<TournamentScreen1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        Container(
-          child: Card(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Tornament1 Name '),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(shrinkWrap: true, children: [
+              const SizedBox(
+                height: 5,
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return TournamentScreen2();
-                    }));
-                  },
-                  child: Text('View Tournament'))
-            ],
-          )),
-        ),
-        Container(
-          child: Card(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Tornament2 Name '),
+              tournament(context),
+              const SizedBox(
+                height: 5,
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return TournamentScreen2();
-                    }));
-                  },
-                  child: Text('View Tournament'))
-            ],
-          )),
-        ),
-        Container(
-          child: Card(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Tornament3 Name '),
+              tournament(context),
+              const SizedBox(
+                height: 5,
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return TournamentScreen2();
-                    }));
-                  },
-                  child: Text('View Tournament'))
-            ],
-          )),
-        ),
-        Container(
-          child: Card(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Tornament4 Name '),
+              tournament(context),
+              const SizedBox(
+                height: 5,
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return TournamentScreen2();
-                    }));
-                  },
-                  child: Text('View Tournament'))
-            ],
-          )),
-        )
-      ]),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+              tournament(context),
+              const SizedBox(
+                height: 5,
+              ),
+            ]),
+          ),
+          Container(
+            height: 50,
+            width: double.infinity,
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                height: 35,
+                width: 150,
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple),
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text('UMPIRE'),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                height: 35,
+                width: 150,
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple),
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text('Tournament'),
+                ),
+              )
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container tournament(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      child: Card(
+          child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Tornament1 Name '),
+          ),
+          const Divider(),
+          Text('No.Of Registered Participants : 5'),
+          Text('Match Date: 26-11-2022'),
+          Text('Status: Pending'),
+          const SizedBox(
+            height: 5,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Colors.green),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return TournamentScreen2();
+                }));
+              },
+              child: Text('View Tournament')),
+          const SizedBox(
+            height: 5,
+          ),
+        ],
+      )),
     );
   }
 }
